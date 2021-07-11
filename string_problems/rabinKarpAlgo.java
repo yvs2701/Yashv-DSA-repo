44933c1771b4c05b237f452bfb9a76447194f7b2
yvs2701 <yashvardhan.ys86@gmail.com>
2021-07-11 19:29:43 +0530

    done Q64 Rabin Karp Algo(single pattern searching)

---
+++ string_problems/rabinKarpAlgo.java
@@ -0,0 +1,73 @@
+package string_problems;
+
+import java.util.Scanner;
+
+public class rabinKarpAlgo {
+   /* Given a text txt[0..n-1] and a pattern pat[0..m-1], write a function search(char pat[], char txt[]) that prints all occurrences of pat[] in txt[]. You may assume that n > m.
+    Input : txt[] = "THIS IS A TEST TEXT", pat[] = "TEST"
+        Output: Pattern found at index 10
+    Input : txt[] =  "AABAACAADAABAABA", pat[] =  "AABA"
+        Output: Pattern found at index 0 Pattern found at index 9 Pattern found at index 12
+    The average and best-case running time of the Rabin-Karp algorithm is O(n+m), but its worst-case time is O(nm). Worst case of Rabin-Karp algorithm occurs when all characters of pattern and text are same as the hash values of all the substrings of txt[] match with hash value of pat[]. For example pat[] = “AAA” and txt[] = “AAAAAAA” */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine(); // enter the text which may contain the pattern to search
+        String pattern = sc.nextLine(); // enter the pattern to search
+        sc.close();
+        searchPattern(text, pattern);
+    }
+    private static void searchPattern(String text, String pattern) {
+        int txt_len = text.length(), pat_len = pattern.length();
+        int hash_pat = 0, hash_txt = 0; // hash values for pattern and text's substrings
+        
+        /* FOR SOME REASON mod = 1e9+7 was not giving the right answer, however, CHANGING IT TO mod = 1e5+7 MADE THE CODE MAGICALLY WORK */
+        final int mod = 100007;         // prime number to calculate modulo... larger modulo denominator reduces collisions in hash
+
+        final int d = 256;              // number of characters possible in a string
+        int coeff = 1;                  // stores the multiplier (or coeffecient) for the first index of the sliding window
+
+        /*HASHING PATTERN:
+        if text     = 'abcd'
+        hashed text = 256^3 *'a' + 256^2 *'b' + 256^1 *'c' + 256^0 *'d'
+        (each character represents its ascii code in the calculation) */
+
+        // The value of coeff would be "(d^(pat_len - 1)) % mod"
+        for (int i = 0; i < pat_len - 1; i++)
+            coeff = (coeff * d) % mod;
+
+        // calculate hash of the first window and the pattern itself
+        for (int i = 0; i < pat_len; i++) {
+            hash_pat = (d * hash_pat + pattern.charAt(i)) % mod;
+            hash_txt = (d * hash_txt + text.charAt(i)) % mod;
+        }
+
+        for (int i = 0; i < txt_len - pat_len; i++) {
+            if (hash_txt == hash_pat) {
+                /* our chances of collisions are quite less (1/mod) so we dont need to recheck the substring
+                int j = 0;
+                for (; j < pat_len; j++)
+                    if (text.charAt(i+j) != pattern.charAt(j)) // pattern not matched even when the hash was same
+                        break;
+
+                if (j == pat_len) // pattern found */
+                    System.out.println("Pattern found at index " + i);
+            }
+            hash_txt = (d * (hash_txt - text.charAt(i) * coeff) + text.charAt(i + pat_len)) % mod; // calculating next window (i+1 th index)
+
+            // We might get negative value of t, converting it to positive
+            if (hash_txt < 0)
+                hash_txt = hash_txt + mod;
+        }
+        if (hash_txt == hash_pat) // checking for the last window
+            System.out.println("Pattern found at index " + (txt_len - pat_len));
+    }
+}
+/* The Naive String Matching algorithm slides the pattern one by one. After each slide, it one by one checks characters at the current shift and if all characters match then prints the match. 
+Like the Naive Algorithm, Rabin-Karp algorithm also slides the pattern one by one. But unlike the Naive algorithm, Rabin Karp algorithm matches the hash value of the pattern with the hash value of current substring of text, and if the hash values match then only it starts matching individual characters. So Rabin Karp algorithm needs to calculate hash values for following strings.
+1) Pattern itself. 
+2) All the substrings of the text of length m. 
+Since we need to efficiently calculate hash values for all the substrings of size m of text, we must have a hash function which has the following property. 
+Hash at the next shift must be efficiently computable from the current hash value and next character in text or we can say 
+hash(text[s+1 .. s+m]) must be efficiently computable from hash(text[s .. s+m-1]) and text[s+m] 
+i.e., hash(text[s+1 .. s+m]) = rehash(text[s+m], hash(txt[s .. s+m-1])) and rehash must be O(1) operation.
+The hash function suggested by Rabin and Karp calculates an integer value. The integer value for a string is the numeric value of a string. */
