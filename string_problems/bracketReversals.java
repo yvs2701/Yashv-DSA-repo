fd4b0209bef08a53cc3777a143e953033d64f4d2
yvs2701 <yashvardhan.ys86@gmail.com>
2021-07-18 18:36:01 +0530

    optimized Q67 minimum number of bracket reversals

--- string_problems/bracketReversals.java
+++ string_problems/bracketReversals.java
@@ -1,7 +1,7 @@
 package string_problems;
 
 import java.util.Scanner;
-import java.util.Stack;
+// import java.util.Stack;
 
 class bracketReversals {
     /* Given a string S consisting of only opening and closing curly brackets '{' and '}', find out the minimum number of 
@@ -22,8 +22,30 @@ class bracketReversals {
         System.out.println(countReversals(input));
     }
 
-    // not optimised uses O(|str|) extra space too. Time Complexity = O(|str|)
+    // Time complexity = O(|str|) and Space complexity = O(1), i.e. constant space
     private static int countReversals(String str) {
+        int open_count = 0, close_count = 0, l = str.length();
+
+        if ((l & 1) != 0) // the length is odd
+            return -1;    // then we simply cannot balance the expression
+
+        for (int i = 0; i < l; i++) {
+            char ch = str.charAt(i);
+            if (ch == '{')
+                open_count++;
+            else {
+                if (open_count == 0)
+                    close_count++;
+                else
+                    open_count--;
+            }
+        }
+
+        // return ceil(m/2) + ceil(n/2)
+        return (int) (Math.ceil((double)open_count / 2) + Math.ceil((double)close_count / 2));
+    }
+    // below approach is not optimised. It uses O(|str|) extra space & Time Complexity is O(|str|)
+    /* private static int countReversals(String str) {
         int open_count = 0, l = str.length();
 
         if ((l & 1) != 0) // the length is odd
@@ -51,5 +73,5 @@ class bracketReversals {
         // return ceil(m/2) + ceil(n/2) which is actually equal to (m+n)/2 + n%2 when m+n is even
         // here m = |closing braces|, n = |opening braces|
         return (l/2 + open_count%2);
-    }
+    } */
 }
