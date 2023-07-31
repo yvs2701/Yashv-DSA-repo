04c8cc83c4c740132f77fbee598bc06b83e9504d
yvs2701 <yashvardhan.ys86@gmail.com>
2023-07-31 17:00:11 +0530

    Longest Common Subsequence

---
+++ Leetcode/minASCIIDelete.java
@@ -0,0 +1,61 @@
+/* Leetcode 712: "Minimum ASCII Delete Sum for Two Strings" - based on longest common subseqeunce.
+Given two strings s1 and s2, return the lowest ASCII sum of deleted characters to make two strings equal.
+Example 1:
+    Input:
+        s1 = "sea", s2 = "eat"
+    Output:
+        231
+    Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum. Deleting "t" from "eat" adds 116 to the sum.
+    At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
+
+Example 2:
+    Input:
+        s1 = "delete", s2 = "leet"
+    Output:
+        403
+    Explanation: Deleting "dee" from "delete" to turn the string into "let", adds 100(d) + 101(e) + 101(e) to the sum.
+    Deleting "e" from "leet" adds 101(e) to the sum. At the end, both strings are equal to "let", and the answer is 100 + 101 + 101 + 101 = 403.
+    If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher. */
+
+class minASCIIDelete {
+    public static void main(String[] args) {
+        String s1 = "helloworld";
+        String s2 = "holaworl";
+        System.out.println(minimumDeleteSum(s1, s2));
+    }
+
+    static int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        //initialize first column with s1 values
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] + s1.charAt(i - 1);
+        }
+
+        //initialize first row with s2 values
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = dp[0][i - 1] + s2.charAt(i - 1);
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // if both characters are equal then use
+                // previous diagonal value which 
+                // contains sum of characters which were not equal on last comparison
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(
+                            dp[i - 1][j] + s1.charAt(i - 1), // delete character from s1
+                            dp[i][j - 1] + s2.charAt(j - 1) // delete character from s2
+                    );
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
